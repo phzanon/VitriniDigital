20ed3045dab1cbb83e68bca1332cbc988fd1733e
phzanon <p.hzanon123@gmail.com>
2023-05-07 21:54:36 -0300

    remove google apikey from code

--- src/app/services/busca-loc.service.ts.service.ts
+++ src/app/services/busca-loc.service.ts.service.ts
@@ -24,25 +24,7 @@ export class BuscaLocServiceTsService {
   constructor(private httpClient: HttpClient,
     private router: Router) {
   }
-
-  buscaLocalizacao(logradouro: string,
-    cidade: string,
-    estado: string) {
-    const URL = 'https://maps.googleapis.com/maps/api/geocode/json';
-    //var geolocalizacao: Geolocalizacao;
-    var location = URL + "?address=" + logradouro + " " + cidade + " " + estado;
-    location = location + "&key=" + "AIzaSyDrWNvYF5ZeWJ2a6W8wYWrmTQx_SyncexU"
-    var geolocalizacao = this.getLocation(location);
-    var geoloc: Geolocalizacao;
-    if(geolocalizacao != null) {
-      geolocalizacao.pipe(map((response) => {
-        geoloc = new Geolocalizacao(response.results[0].location.lat, 
-          response.results[0].location.lng)
-          this.locations.push(geoloc);
-      })); 
-    }
-  }
-
+  
   getLocation(url: string): Observable<ResponseGoogleMaps> {
     return this.httpClient.get<ResponseGoogleMaps>(url).pipe(
       tap((resposta) => {
