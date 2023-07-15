c902711c63ff643dfa222d168f1682df215cb063
Rodolfo Mathias <rodolfo_psl@hotmail.com>
2023-07-15 12:08:32 -0300

    correçao bug input

--- src/app/pages/mostrar-dados/mostrar-dados.component.ts
+++ src/app/pages/mostrar-dados/mostrar-dados.component.ts
@@ -7,15 +7,22 @@ import { EstabelecimentosService } from 'src/app/services/estabelecimentos.servi
   templateUrl: './mostrar-dados.component.html',
   styleUrls: ['./mostrar-dados.component.css']
 })
-export class MostrarDadosComponent implements OnInit{
+export class MostrarDadosComponent implements OnInit {
 
   public usuarios$: Estabelecimento[] = [];
 
-  constructor(private estabelecimentoService: EstabelecimentosService){}
+  constructor(private estabelecimentoService: EstabelecimentosService) { }
 
   ngOnInit(): void {
     var est = this.estabelecimentoService.buscarEstabelecimentoPorId(`${localStorage.getItem('idEstabelecimento')}`).subscribe(
       (res) => {
+        if (res.telefone2 == "undefined")
+          res.telefone2 = "";
+        if (res.endereco.complemento == "undefined")
+          res.endereco.complemento = "";
+        if (res.endereco.pontoReferencia == "undefined")
+          res.endereco.pontoReferencia = "";
+
         this.usuarios$.push(res);
       }
     );
